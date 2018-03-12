10d4949cdfd0ea2ca824f73eb7906906a751135a
Guest User <Guest@Epicodus-3D.local>
2018-03-12 13:07:58 -0700

    add everything

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
-import { pingPong } from './ping-pong';
+import { Entry } from './entry';
 import './styles.css';
 
 $(document).ready(function() {
-  $('#ping-pong-form').submit(function(event) {
+  $('#entry-form').submit(function(event) {
     event.preventDefault();
-    var goal = $('#goal').val();
-    var output = pingPong(goal);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
-    });
+    var title = $('#title').val();
+    var body = $('#body').val();
+    var time = Date.now().toString();
+    var timestamp = Date(time);
+    var entry = new Entry(title, body, timestamp);
+
+    $('#solution').append("<h2>Title: " + entry.title + "</h2><p>Body: " + entry.body + "</p>" + "<li>Word Count: " + entry.word_count() + "</li><li>Vowel Count: " + entry.vowel_count() + "</li><li>Consonant Count: " + entry.consonant_count() + "</li><li>Time Stamp: " + entry.timestamp);
+    console.log(entry.word_count);
   });
 });
