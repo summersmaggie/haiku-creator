a6151412af173304e9b205a0a68b72b8c0cf70c3
Hannah Lee and Maggie Summers <hannah@>
2018-03-14 10:46:11 -0700

    add for loop to test for two consonants in a row

--- spec/haiku-spec.js
+++ spec/haiku-spec.js
@@ -2,9 +2,15 @@ import { Haiku } from  '../src/haiku.js';
 
 describe('Haiku', function() {
   it('should test whether an input has three lines', function() {
-    var newHaiku = new Haiku("lineOne", "lineTwo", "lineThree")
+    let newHaiku = new Haiku("lineOne", "lineTwo", "lineThree")
     expect(newHaiku.lineOne).toEqual("lineOne")
     expect(newHaiku.lineTwo).toEqual("lineTwo")
     expect(newHaiku.lineThree).not.toEqual(" ")
   });
+
+  it('should test whether each line has more than two consonants in a row', function() {
+    let newHaiku = new Haiku("this buffet is awesome", "lineTwo", "lineThree")
+    expect(newHaiku.checkConsonant("this buffet is awesome")).toEqual(true)
+  });
+
 });
--- src/haiku.js
+++ src/haiku.js
@@ -1,5 +1,21 @@
-export function Haiku(lineOne, lineTwo, lineThree) {
-  this.lineOne = lineOne;
-  this.lineTwo = lineTwo;
-  this.lineThree = lineThree;
+export class Haiku {
+  constructor(lineOne, lineTwo, lineThree) {
+    this.lineOne = lineOne;
+    this.lineTwo = lineTwo;
+    this.lineThree = lineThree;
+}
+
+  checkConsonant(lineOne) {
+
+    let lineArray = [];
+    lineArray = lineOne.split(" ");
+    console.log(lineArray);
+    let arrayLength = lineArray.length
+
+    for (let i = 0; i < arrayLength; i++) {
+      if (/[qwrtypsdfghjklzxcvbnm]{2,}/i.test(lineArray[i])) {
+        return true;
+      }
+    }
+  }
 }
